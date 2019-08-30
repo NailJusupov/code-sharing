4ee2a3df06bad3aa952c1e11b03d5164031cd98e
naiu0819 <nail1999usupov@mail.ru>
2019-08-30 17:56:26 +0400

    Fixed bug with unauthorized login request

--- src/main/java/ru/study/codesharing/configs/WebSecurityConfig.java
+++ src/main/java/ru/study/codesharing/configs/WebSecurityConfig.java
@@ -1,17 +1,13 @@
 package ru.study.codesharing.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import ru.study.codesharing.repositories.UsersRepository;
-import ru.study.codesharing.security.EmailAndPasswordAuthenticationProvider;
 import ru.study.codesharing.security.NonRedirectingAuthenticationSuccessHandler;
 
 import javax.servlet.http.HttpServletResponse;
@@ -32,11 +28,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    AuthenticationProvider authenticationProvider(UsersRepository usersRepository, PasswordEncoder passwordEncoder) {
-        return new EmailAndPasswordAuthenticationProvider(usersRepository, passwordEncoder);
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -47,9 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginProcessingUrl("/api/auth/login")
                 .successHandler(nonRedirectingAuthenticationSuccessHandler)
-                .failureHandler((httpServletRequest, httpServletResponse, e) -> {
-                    httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "error here");
-                })
+                .failureHandler((httpServletRequest, httpServletResponse, e) -> {httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);})
                 .and()
                 .logout()
                 .logoutUrl("/api/auth/logout")
